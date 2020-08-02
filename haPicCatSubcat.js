7b66cf5aed02710782be16ed9c8d0b2a8d3298ae
havanuart <havanuart@gmail.com>
2020-08-03 03:46:57 +0530

    havanuart product rel 3.55 commit

---
+++ haPicCatSubcat.js
@@ -0,0 +1,5 @@
+ReadPicCatSubcatJSONFile([
+ {  "category" : "charcoal" , "subcategory" : ["botanical", "abstract", "still life", "marine", "pet animal"]  }, 
+ {  "category" : "oil painting" , "subcategory" : ["impression", "textured abstract", "abstract", "palette knife", "scenary"]  }, 
+ {  "category" : "oil charcoal" , "subcategory" : ["mixed media"]  } 
+]);
---
+++ haPicSubcatIdx.js
@@ -0,0 +1,13 @@
+ReadPicSubcatIdxJSONFile([
+ {  "category" : "charcoal" , "subcategory" : "botanical" , "picarray" : ["0", "1", "3", "4", "6", "8", "12", "14", "17", "19", "20", "25"] }, 
+ {  "category" : "oil painting" , "subcategory" : "impression" , "picarray" : ["2", "11"] }, 
+ {  "category" : "charcoal" , "subcategory" : "abstract" , "picarray" : ["5", "21"] }, 
+ {  "category" : "oil painting" , "subcategory" : "textured abstract" , "picarray" : ["7"] }, 
+ {  "category" : "oil painting" , "subcategory" : "abstract" , "picarray" : ["9", "10", "27"] }, 
+ {  "category" : "charcoal" , "subcategory" : "still life" , "picarray" : ["13"] }, 
+ {  "category" : "oil painting" , "subcategory" : "palette knife" , "picarray" : ["15", "16"] }, 
+ {  "category" : "oil charcoal" , "subcategory" : "mixed media" , "picarray" : ["18"] }, 
+ {  "category" : "oil painting" , "subcategory" : "scenary" , "picarray" : ["22"] }, 
+ {  "category" : "charcoal" , "subcategory" : "marine" , "picarray" : ["23"] }, 
+ {  "category" : "charcoal" , "subcategory" : "pet animal" , "picarray" : ["24", "26"] } 
+]);
